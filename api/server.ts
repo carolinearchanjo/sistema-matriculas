2084b6a00a6b5c978df9a55fb59f45d9afaacd0a
Caroline Archanjo <caroline.archanjo23@gmail.com>
2026-03-03 11:41:58 -0300

    feat: rotas PUT e DELETE para gerenciamento de matrículas

--- api/server.ts
+++ api/server.ts
@@ -73,6 +73,44 @@ app.get("/matriculas", async function (req, res) {
   }
 });
 
+app.delete("/matricula/:id", async function (req, res) {
+    
+  try {
+        await pool.query(
+            "DELETE FROM matriculas WHERE id = $1",
+            [req.params.id]
+        )
+        res.json({ mensagem: "Matrícula deletada com sucesso!" })
+    } catch (erro) {
+        console.error(erro)
+        res.status(500).json({ mensagem: "Erro interno do servidor" })
+    }
+})
+
+app.put("/matricula/:id", async function (req, res) {
+  
+  const dados: Matricula = req.body;
+
+  if (!dados.nome || !dados.email || !dados.curso) {
+    res.status(400).json({ mensagem: "Todos os campos são obrigatórios" });
+    return;
+  }
+
+  try {
+    const atualizaDados = await pool.query(
+      "UPDATE matriculas SET nome = $1, email = $2, curso = $3 WHERE id = $4 RETURNING *",
+      [dados.nome, dados.email, dados.curso, req.params.id],
+    );
+    res.status(201).json({
+      mensagem: "Matrícula atualizada com sucesso!",
+      dados: atualizaDados.rows[0],
+    });
+  } catch (erro) {
+    console.error(erro);
+    res.status(500).json({ mensagem: "Erro interno do servidor" });
+  }
+});
+
 app.listen(3000, function () {
   console.log("Servidor rodando na porta 3000");
 });
